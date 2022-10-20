669046d9afadc50b6f81320eb0432cb550300c23
IsaacFaf <2163607@cmontmorency.qc.ca>
2022-10-19 23:42:11 -0400

    chaptersObj
    Modif dans l'index + Css instauration de la classe imgchange mais pas encore implanté. Image link sur les chapitres

--- START.js
+++ START.js
@@ -1,5 +1,7 @@
-START = {
+let chaptersObj = {
+START: {
     subtitle:"START",
+    img: "assets/img/debut.jpg",
     text:"Vous arrivez enfin à vos vacances d'été! Comme à chaque année, vous commencez toujours vos vacances avec un RoadTrip pour déconnecter du travail. Vous avez donc fini votre valise et êtes prêt à partir. À l'instant de barrer la porte, vous apercevez une hache. Vous vous posez la question s'il serait préférable de l'apporter pendant le RoadTrip.",
     options:[PasHache = {
         text:"Ne pas prendre la hache",
@@ -10,8 +12,9 @@ START = {
     }],
 },
 
-PasHache = {
+PasHache: {
     subtitle:"PasHache",
+    img:"assets/img/oursbloque.jpg",
     text:"Vous avez décidé de ne pas prendre la hache. Vous avez débuté votre RoadTrip et tout semble bien se dérouler jusqu'à ce qu'un ours vous bloque la route. Impossible de l'éviter ou de faire marche arrière, il vous restes pas beaucoup d'option et il faut choisir vite!!",
     options:[DefierCorp = {
         text:"Défiez l'ours au corp à corp",
@@ -22,8 +25,9 @@ PasHache = {
     }],
 },
 
-DefierCorp = {
+DefierCorp: {
     subtitle:"DefierCorp",
+    img:"assets/img/ContourOuEau.jpg",
     text:"Vous avez décidé de le défier en corp à corp. Après une heure de combat acharné, vous êtes tous les deux en mauvaise état mais il te bloque toujours la route. Vous n'avez plus le choix, il vous faut fuir. Mais comment?",
     options:[ChaletAbandon = {
         text:"Longez le lac par la forêt",
@@ -33,8 +37,9 @@ DefierCorp = {
         action:"goToChapter('ChaletAbandon')"
     }],
 },
-ChaletAbandon = {
+ChaletAbandon: {
     subtitle:"ChaletAbandon",
+    img:"assets/img/ContourOuEau.jpg",
     text:"Vous avez réussi a vous enfuir mais vous avez besoin de manger et de vous mettre à l'abri rapidement. Vous voyez au loin un chalet et un bannanier. Serait-il plus sécuritaire de se réfugier dans le chalet en premier?",
     options:[Banane = {
         text:"Décidez d'allez chercher quelques bananes se qui vous aidera à vous rétablir.",
@@ -44,16 +49,18 @@ ChaletAbandon = {
         action:"goToChapter('Chalet')"
     }],
 },
-Banane = {
+Banane: {
     subtitle:"Banane",
+    img:"assets/img/banannier.jpg",
     text:"Vous avez aperçu l'ours en essayant de vous cacher. Vous êtes tombé sur une banane. MORT. ",
     options:[MortBanane = {
         text:"Recommencez pour avoir une fin différente",
         action:"goToChapter('START')"
     }],
 },
-AvecHache = {
+AvecHache: {
     subtitle:"AvecHache",
+    img:"assets/img/oursbloque.jpg",
     text:"Vous avez décidé de prendre la hache. Vous avez débuté votre RoadTrip et tout semble bien se dérouler jusqu'à ce qu'un ours vous bloque la route. Impossible de l'éviter ou de faire marche arrière, il vous restes pas beaucoup d'option et il faut choisir vite!!",
     options:[OffrirVin1 = {
         text:"Lui offrir un verre de vin",
@@ -63,24 +70,27 @@ AvecHache = {
         action:"goToChapter('AttaqueHache')"
     }],
 },
-AttaqueHache = {
+AttaqueHache: {
     subtitle:"AttaqueHache",
+    img:"assets/img/mortOursBloque.jpg",
     text:"L'ours s'est énervé et votre hache n'a pas suffit à gagner votre combat. Vous êtes mort.",
     options:[MortHache = {
         text:"Recommencez pour trouvez d'autres fins",
         action:"goToChapter('START')"
     }],
 },
-OffrirVin = {
+OffrirVin: {
     subtitle:"OffrirVin",
+    img:"assets/img/VinOurs.jpg",
     text:"L'ours, quoique surpris, accepte l'offre et vous apprenez à vous connaître. L'ours semble bien vous apprécier et il vous a obligé de continuer votre RoadTrip avec lui.",
     options:[OursRoadTrip = {
         text:"Continuer le RoadTrip avec l'ours",
         action:"goToChapter('OursRoadTrip')"
     }],
 },
-OursRoadTrip = {
+OursRoadTrip: {
     subtitle:"OursRoadTrip",
+    img:"assets/img/chasseur.jpg",
     text:"En continuant votre RoadTrip avec l'ours, vous apercevez au loin une cabane de chasseur qui semble habité. Impossible de passer innaperçu sans leurs attirer l'attention.",
     options:[SauveOurs = {
         text:"Essayez de sauver l'ours",
@@ -94,30 +104,32 @@ OursRoadTrip = {
         action:"vinTrouver"
     }],
 },
-SauveOurs = {
+SauveOurs: {
     subtitle:"SauveOurs",
+    img:"assets/img/OursDeuise.jpg",
     text:"Déguiser l'ours comme votre compagne à fonctionné! En continuant le Road Trip ensemble, vous avez trouvé encore plus de vin et puisque l'ours a bien aimer votre vin. Il décide donc de vous tuer pour en avoir plus pour lui. MORT",
     options:[MortSauveOurs = {
         text:"Essayez de trouver d'autres fins. (VOUS AVEZ TROUVEZ (+) DE VINS! UTILE POUR UNE FIN CACHÉE)",
         action:"goToChapter('START')"
     }],
 },
-NuireOurs = {
+NuireOurs: {
     subtitle:"NuireOurs",
+    img:"assets/img/hunting.jpg",
     text:"Vous attirez l'attention des chasseurs, C'est votre RoadTrip en solo après tout ! Mais il semblerait que avoir crier a fait peur aux chasseurs. Vous avez reçu une balle de fusil de chasse. MORT",
     options:[MortNuireOurs = {
         text:"Réessayez pour trouver d'autres fins",
         action:"goToChapter('START')"
     }],
 },
-LOCKVinChasseur = {
+LOCKVinChasseur: {
     subtitle:"LOCKVinChasseur",
     text:"Vous êtes rendu un groupe à faire le Road Trip. L'ours s'entend super bien avec les chasseurs. Vous decidez de vivre tous ensemble puisque vous vous entendez bien autour d'une bonne bouteille de vin! FIN!",
     options:[MeilleurFin = {
         text:"Vous avez trouvé la meilleure fin mais vous pouvez toutefois recommencer et trouver les autres fins!",
         action:"goToChapter('START')"
     }],
-}
+}}
 
 
 
